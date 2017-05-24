42d088102f93999f79e12521d743d64adc188a48
hwing <bookbank1@snu.ac.kr>
2017-05-24 22:10:49 +0900

    Initial Commit

--- comp_AE_model.py
+++ comp_AE_model.py
@@ -25,12 +25,13 @@ class AE(object):
         self.input = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, self.image_dim])
         self.gt = tf.placeholder(tf.float32, [None, self.output_size, self.output_size, self.output_dim])
         self.batch_size = tf.shape(self.input)[0]
+        self.is_training = tf.placeholder(tf.bool)
 
-        embed = self.encoder(self.input)
-        gen = self.generator(embed)
+        self.embed = self.encoder(self.input, self.is_training)
+        self.gen = self.generator(self.embed, self.is_training)
 
         # L1 Loss
-        self.loss = tf.reduce_mean(tf.abs(self.gt - gen))
+        self.loss = tf.reduce_mean(tf.abs(self.gt - self.gen))
         # self.loss = tf.nn.sigmoid_cross_entropy_with_logits(gen, self.gt)
         self.loss_sum = tf.summary.scalar("loss", self.loss)
         self.vars = tf.trainable_variables()
@@ -79,7 +80,8 @@ class AE(object):
         _deconv = tf.nn.bias_add(_deconv, _biases)
         _bn = slim.batch_norm(_deconv, decay=_bn_params['decay'], epsilon=_bn_params['epsilon'],
                               param_initializers=_bn_params['param_initializers'],
-                              updates_collections=_bn_params['updates_collections'])
+                              updates_collections=_bn_params['updates_collections'],
+                              is_training=_bn_params['is_training'])
         if ~(_last):
             return tf.nn.relu(_bn)
         else :
@@ -103,7 +105,8 @@ class AE(object):
             _d0 = tf.reshape(_embed, [self.batch_size, 3, 3, fc_d/2])
             _bn_d0 = slim.batch_norm(_d0, decay=bn_params['decay'], epsilon=bn_params['epsilon'],
                                   param_initializers=bn_params['param_initializers'],
-                                  updates_collections=bn_params['updates_collections'])
+                                  updates_collections=bn_params['updates_collections'],
+                                     is_training=bn_params['is_training'])
             with tf.variable_scope("deconv1"):
                 _d1 = self.deconv(_bn_d0, [self.batch_size, 6, 6, fc_d/4], [3, 3, fc_d/4, fc_d/2], [fc_d/4], bn_params)
             with tf.variable_scope("deconv2"):
@@ -122,10 +125,20 @@ class AE(object):
         cwd = os.getcwd()
         trainimg = np.load(cwd+'/PASCAL_VOC_2012/0.2/compoundData_train_aug.npy')
         trainlabel = np.load(cwd + '/PASCAL_VOC_2012/0.2/compoundData_seg_train_aug.npy').astype(float)
+        trainimg = np.where(trainimg<0., 0., trainimg)
+        trainimg = np.where(trainimg > 1., 1., trainimg)
         ds_trainimg = np.copy(trainimg)         # for display, no shuffle
         ds_trainlabel = np.copy(trainlabel)     # for display, no shuffle
         testimg = np.load(cwd+'/PASCAL_VOC_2012/0.2/compoundData_test.npy')
         testlabel = np.load(cwd + '/PASCAL_VOC_2012/0.2/compoundData_seg_test.npy').astype(float)
+        testimg = np.where(testimg < 0., 0., testimg)
+        testimg = np.where(testimg > 1., 1., testimg)
+        trainimg=trainimg[:4800]
+        trainlabel=trainlabel[:4800]
+        trainimg= np.concatenate((trainimg,testimg[:800]),axis=0)
+        trainlabel = np.concatenate((trainlabel, testlabel[:800]),axis=0)
+        testimg = testimg[800:]
+        testlabel = testlabel[800:]
         didx = np.random.randint(64, size=5)
 
         optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1).minimize(self.loss, var_list=self.vars)
@@ -158,11 +171,15 @@ class AE(object):
                 batch_gt = trainlabel[range(idx * config.batch_size, (idx + 1) * config.batch_size)]
                 random_flip = np.random.random_sample()
                 if random_flip > 0.5:
-                    batch_files = np.fliplr(batch_files)
-                    batch_gt = np.fliplr(batch_gt)
+                    for batch in range(config.batch_size):
+                        batch_files[batch, :, : ,:] = np.fliplr(batch_files[batch, :, : ,:])
+                        batch_gt[batch, :, :, :] = np.fliplr(batch_gt[batch, :, :, :])
+
+                    # batch_files = np.fliplr(batch_files)
+                    # batch_gt = np.fliplr(batch_gt)
 
                 # Update network
-                _, summary_str = self.sess.run([optim, self.sum], feed_dict={self.input: batch_files, self.gt: batch_gt})
+                _, summary_str = self.sess.run([optim, self.sum], feed_dict={self.input: batch_files, self.gt: batch_gt, self.is_training: True})
                 self.writer.add_summary(summary_str, counter)
 
             if np.mod(epoch, 200) == 1:
@@ -179,16 +196,16 @@ class AE(object):
         _train_xs = np.reshape(_trainimg, (-1, 21, 21, 300))
         _train_ys = np.reshape(_trainlabel, (-1, 21, 21, 1))
         _train_total = np.shape(_train_xs)[0]
-        train_acc_embed = self.encoder(self.input, is_training=False, reuse=True)
-        train_acc_gen = self.generator(train_acc_embed, is_training=False, reuse=True)
-        img_gen = self.sess.run(train_acc_gen,
-                                feed_dict={self.input: _train_xs})
+        # train_acc_embed = self.encoder(self.input, is_training=False, reuse=True)
+        # train_acc_gen = self.generator(train_acc_embed, is_training=False, reuse=True)
+        train_img_gen, train_loss = self.sess.run([self.gen, self.loss], feed_dict={self.input: _train_xs, self.gt: _train_ys, self.is_training: False})
+                                # feed_dict={self.input: _train_xs})
         train_score = 0
         for idx_acc_train in xrange(_train_total):
             img_score = 0
             for r1 in xrange(21):
                 for c1 in xrange(21):
-                    if np.abs(img_gen[idx_acc_train, r1, c1, 0]-_train_ys[idx_acc_train, r1, c1, 0]) < 0.5:
+                    if np.abs(train_img_gen[idx_acc_train, r1, c1, 0]-_train_ys[idx_acc_train, r1, c1, 0]) < 0.5:
                         img_score += 1
             img_score = img_score/(21.*21.)
             train_score = train_score + img_score
@@ -197,16 +214,16 @@ class AE(object):
         _test_xs = np.reshape(_testimg, (-1, 21, 21, 300))
         _test_ys = np.reshape(_testlabel, (-1, 21, 21, 1))
         _test_total = np.shape(_test_xs)[0]
-        test_acc_embed = self.encoder(self.input, is_training=False, reuse=True)
-        test_acc_gen = self.generator(test_acc_embed, is_training=False, reuse=True)
-        img_gen = self.sess.run(test_acc_gen,
-                                feed_dict={self.input: _test_xs})
+        # test_acc_embed = self.encoder(self.input, is_training=False, reuse=True)
+        # test_acc_gen = self.generator(test_acc_embed, is_training=False, reuse=True)
+        test_img_gen, test_loss, aa = self.sess.run([self.gen, self.loss, self.embed], feed_dict={self.input: _test_xs, self.gt: _test_ys, self.is_training: False})
+                                # feed_dict={self.input: _test_xs})
         test_score = 0
         for idx_acc_test in xrange(_test_total):
             test_img_score = 0
             for r2 in xrange(21):
                 for c2 in xrange(21):
-                    if np.abs(img_gen[idx_acc_test, r2, c2, 0] - _train_ys[idx_acc_test, r2, c2, 0]) < 0.5:
+                    if np.abs(test_img_gen[idx_acc_test, r2, c2, 0] - _train_ys[idx_acc_test, r2, c2, 0]) < 0.5:
                         test_img_score += 1
             test_img_score = test_img_score / (21. * 21.)
             test_score = test_score + test_img_score
@@ -214,22 +231,23 @@ class AE(object):
 
         # Calculate loss for training set & test set
 
-        train_feeds = {self.input: _train_xs, self.gt: _train_ys}
-        train_loss = self.sess.run(self.loss, feed_dict=train_feeds)
-        test_feeds = {self.input: _test_xs, self.gt: _test_ys}
-        test_loss = self.sess.run(self.loss, feed_dict=test_feeds)
-        print("Epoch: [%04d/%04d] train accuracy: %.9f, train loss: %.9f, train accuracy: %.9f, test loss: %.9f" % (_epoch + 1, total_epochs, train_score, train_loss, test_score, test_loss))
+        # train_feeds = {self.input: _train_xs, self.gt: _train_ys, self.is_training: False}
+        # train_loss = self.sess.run(self.loss, feed_dict=train_feeds)
+        # test_feeds = {self.input: _test_xs, self.gt: _test_ys, self.is_training: False}
+        # test_loss = self.sess.run(self.loss, feed_dict=test_feeds)
+        print("Epoch: [%04d/%04d] train accuracy: %.9f, train loss: %.9f, test accuracy: %.9f, test loss: %.9f" % (_epoch + 1, total_epochs, train_score, train_loss, test_score, test_loss))
 
 
         # Test image, Training image plotting
-        if np.mod(_epoch,50) == 0:
+        if np.mod(_epoch,5) == 0:
             plt.figure(1)
 
             for dtest in range(5):
-                d_embed = self.encoder(self.input, is_training=False, reuse=True)
-                d_gen = self.generator(d_embed, is_training=False, reuse=True)
-                c_out = self.sess.run(d_gen,
-                                 feed_dict={self.input: np.reshape(_testimg[_didx[dtest], :, :, :], (-1, 21, 21, 300))})
+                # d_embed = self.encoder(self.input, is_training=False, reuse=True)
+                # d_gen = self.generator(d_embed, is_training=False, reuse=True)
+                c_out, bb= self.sess.run([self.gen,self.embed],
+                                          feed_dict={self.input:np.reshape(_test_xs[_didx[dtest]], (-1, 21, 21, 300)), self.is_training: False})
+                                 # feed_dict={self.input: np.reshape(_testimg[_didx[dtest], :, :, :], (-1, 21, 21, 300))})
                 a = plt.subplot(2, 5, dtest + 1)
                 a.matshow(np.reshape(_testlabel[_didx[dtest], :, :, :], (21, 21)), cmap='gray')
                 b = plt.subplot(2, 5, dtest + 5 + 1)
@@ -243,12 +261,14 @@ class AE(object):
             plt.figure(1).savefig(os.path.join(test_dir, test_fig))
             # plt.figure(1).savefig(os.getcwd() + '/data/output_figure/test/test_%04d' % (_epoch + 1))
 
+
             plt.figure(2)
             for dtrain in range(5):
-                d_embed = self.encoder(self.input, is_training=False, reuse=True)
-                d_gen = self.generator(d_embed, is_training=False, reuse=True)
-                c_out = self.sess.run(d_gen,
-                               feed_dict={self.input: np.reshape(_trainimg[_didx[dtrain], :, :, :], (-1, 21, 21, 300))})
+                # d_embed = self.encoder(self.input, is_training=False, reuse=True)
+                # d_gen = self.generator(d_embed, is_training=False, reuse=True)
+                c_out = self.sess.run(self.gen,
+                                      feed_dict={self.input:np.reshape(_train_xs[_didx[dtrain]], (-1, 21, 21, 300)), self.is_training: False})
+                               # feed_dict={self.input: np.reshape(_trainimg[_didx[dtrain], :, :, :], (-1, 21, 21, 300))})
                 a = plt.subplot(2, 5, dtrain + 1)
                 a.matshow(np.reshape(_trainlabel[_didx[dtrain], :, :, :], (21, 21)), cmap='gray')
                 b = plt.subplot(2, 5, dtrain + 5 + 1)
@@ -256,11 +276,42 @@ class AE(object):
 
             plt.title("TRAIN")
             train_dir = os.path.join(save_dir, "output_figure_recent/train")
-            train_fig = "train_%04d" % (_epoch + 1)
+            train_fig = "train_train%04d" % (_epoch + 1)
             if not os.path.exists(train_dir):
                 os.makedirs(train_dir)
             plt.figure(2).savefig(os.path.join(train_dir, train_fig))
-            # plt.figure(2).savefig(os.getcwd() + '/data/output_figure/train/train_%04d' % (_epoch + 1))
+
+            plt.figure(3)
+
+            for dtest in range(5):
+                c_out = test_img_gen[_didx[dtest]]
+                a = plt.subplot(2, 5, dtest + 1)
+                a.matshow(np.reshape(_test_ys[_didx[dtest], :, :, :], (21, 21)), cmap='gray')
+                b = plt.subplot(2, 5, dtest + 5 + 1)
+                b.matshow(np.reshape(c_out, (21, 21)), cmap='gray')
+
+            plt.title("TEST")
+            test_dir = os.path.join(save_dir, "output_figure_recent/test")
+            test_fig = "test_test%04d" % (_epoch + 1)
+            if not os.path.exists(test_dir):
+                os.makedirs(test_dir)
+            plt.figure(3).savefig(os.path.join(test_dir, test_fig))
+
+            plt.figure(4)
+
+            for dtest in range(5):
+                c_out = train_img_gen[_didx[dtest]]
+                a = plt.subplot(2, 5, dtest + 1)
+                a.matshow(np.reshape(_train_ys[_didx[dtest], :, :, :], (21, 21)), cmap='gray')
+                b = plt.subplot(2, 5, dtest + 5 + 1)
+                b.matshow(np.reshape(c_out, (21, 21)), cmap='gray')
+
+            plt.title("TRAIN")
+            test_dir = os.path.join(save_dir, "output_figure_recent/train")
+            test_fig = "train%04d" % (_epoch + 1)
+            if not os.path.exists(test_dir):
+                os.makedirs(test_dir)
+            plt.figure(4).savefig(os.path.join(test_dir, test_fig))
 
     def save(self, checkpoint_dir, step, config):
         model_name = "comp_AE.model"
