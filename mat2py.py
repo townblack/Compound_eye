a03c68f2a0fbf61178ce070aec0233dffd5c3cd9
Hwiyeon <gnldusdlek@gmail.com>
2017-06-07 21:47:57 +0900

    region ground truth

--- mat2py.py
+++ mat2py.py
@@ -7,7 +7,7 @@ import h5py
 cwd = os.getcwd()
 # compoundData = h5py.File(cwd+"/PASCAL_VOC_2012/compoundData_test.mat")
 # compoundData.keys()
-compoundData_seg = h5py.File(cwd+"/PASCAL_VOC_2012/compoundData_seg_test.mat")
+compoundData_seg = h5py.File(cwd+"/PASCAL_VOC_2012/compoundData_seg_train.mat")
 compoundData_seg.keys()
 
 # compoundData = sio.loadmat(cwd+"/PASCAL_VOC_2012/compoundData_train_color.mat")
@@ -23,11 +23,17 @@ compoundData_seg = compoundData_seg['compoundData']
 compoundData_seg = np.transpose(compoundData_seg, (0, 1, 3, 4, 2))
 compoundData_rs = np.reshape(compoundData_seg, (np.shape(compoundData_seg)[0],np.shape(compoundData_seg)[1], -1))
 compoundData_gt = np.mean(compoundData_rs, axis=2)
-compoundData_gt = [np.copy(compoundData_gt>0.5)]
+gt_single = np.copy(compoundData_gt)
+neighbor = np.load(os.getcwd()+"/PASCAL_VOC_2012/NN_441.npy")
+for eye in range(np.shape(compoundData_gt)[1]):
+    region_val = np.mean(gt_single[:, neighbor[eye]], axis=1)
+    compoundData_gt[:,eye] = region_val
+
+compoundData_gt = [np.copy(compoundData_gt>0.7)]
 # compoundData_gt_2 = [np.copy(compoundData_gt>0.7)]
 compoundData_gt = np.transpose(compoundData_gt, (1, 2, 0))
 
 # np.save(cwd+'/PASCAL_VOC_2012/compoundData_test.npy', compoundData)
-np.save(cwd+'/PASCAL_VOC_2012/compoundData_seg_test.npy', compoundData_gt)
+np.save(cwd+'/PASCAL_VOC_2012/compoundData_seg_train.npy', compoundData_gt)
 
 print("Process Finished")
